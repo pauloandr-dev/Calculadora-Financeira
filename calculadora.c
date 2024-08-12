f31a7b8bca632b040718173a943cfd408a6cb714
Pauloandr_ <117040493+pauloandr-dev@users.noreply.github.com>
2024-08-11 21:56:23 -0300

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,1 +1,21 @@
+#include "calculadora.h"
 
+double soma(double a, double b) {
+    return a + b;
+}
+
+double subtracao(double a, double b) {
+    return a - b;
+}
+
+double multiplicacao(double a, double b) {
+    return a * b;
+}
+
+double divisao(double a, double b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        return 0;
+    }
+}
