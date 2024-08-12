c7964664bfe48bc723282a4fb0a39a9a5a81c5aa
Pauloandr_ <117040493+pauloandr-dev@users.noreply.github.com>
2024-08-12 14:01:08 -0300

    Update calculadora.h

--- calculadora.h
+++ calculadora.h
@@ -6,3 +6,6 @@ double soma(double a, double b);
 double subtracao(double a, double b);
 double multiplicacao(double a, double b);
 double divisao(double a, double b);
+
+// Funções financeiras
+double calcularJurosSimples(double capital, double taxa, int tempo);
