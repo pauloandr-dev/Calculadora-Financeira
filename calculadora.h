7a91a4719ae840b2a533da4cf868886071c44b9f
Pauloandr_ <117040493+pauloandr-dev@users.noreply.github.com>
2024-08-11 21:55:57 -0300

    Update calculadora.h

--- calculadora.h
+++ calculadora.h
@@ -1,1 +1,8 @@
+#ifndef CALCULADORA_H
+#define CALCULADORA_H
 
+// Operações básicas
+double soma(double a, double b);
+double subtracao(double a, double b);
+double multiplicacao(double a, double b);
+double divisao(double a, double b);
