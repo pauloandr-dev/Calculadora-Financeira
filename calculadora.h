f2a355fc5b26d4d8880d03aefb4b1378d2260f4c
Pauloandr_ <117040493+pauloandr-dev@users.noreply.github.com>
2024-08-12 15:09:14 -0300

    Update calculadora.h

--- calculadora.h
+++ calculadora.h
@@ -9,3 +9,8 @@ double divisao(double a, double b);
 
 // Funções financeiras
 double calcularJurosSimples(double capital, double taxa, int tempo);
+double calcularJurosCompostos(double capital, double taxa, int tempo);
+double converterTaxa(double taxa, int periodos);
+double simularInvestimento(double capital, double taxa, int tempo);
+
+#endif
