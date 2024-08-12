2db443687e6a66f38c94dbebfa1ced06fd0e0c40
Pauloandr_ <117040493+pauloandr-dev@users.noreply.github.com>
2024-08-11 21:57:29 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+#include "calculadora.h"
 
+void menuPrincipal() {
+    int opcao;
+    double a, b;
+    double capital, taxa;
+    int tempo;
+
+    while (1) {
+        printf("Calculadora Financeira\n");
+        printf("1. Soma\n");
+        printf("2. Subtração\n");
+        printf("3. Multiplicação\n");
+        printf("4. Divisão\n");
+        printf("5. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+     switch(opcao) {
+            case 1:
+                printf("Digite os dois números: ");
+                scanf("%lf %lf", &a, &b);
+                printf("Resultado: %.2lf\n", soma(a, b));
+                break;
+            case 2:
+                printf("Digite os dois números: ");
+                scanf("%lf %lf", &a, &b);
+                printf("Resultado: %.2lf\n", subtracao(a, b));
+                break;
+            case 3:
+                printf("Digite os dois números: ");
+                scanf("%lf %lf", &a, &b);
+                printf("Resultado: %.2lf\n", multiplicacao(a, b));
+                break;
+            case 4:
+                printf("Digite os dois números: ");
+                scanf("%lf %lf", &a, &b);
+                printf("Resultado: %.2lf\n", divisao(a, b));
+                break;
+            case 5:
+                printf("Saindo...\n");
+                return;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+                break;
+        }
+    }
+}
+
+int main() {
+    menuPrincipal();
+    return 0;
+}
